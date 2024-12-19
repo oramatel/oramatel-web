ab8212cc6d5c86252f4a2923238cbd251a3b2dfc
unknown <oramatel@gmail.com>
2024-12-19 17:39:53 +1100

    fixed mobile navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -81,8 +81,14 @@ export function Header() {
           </Link>
         </Button>
       </div>
-
-      <MobileNavbar />
+      <MobileNavbar>
+               <Link href="/" className="block p-2">Home</Link>
+               <Link href="/about" className="block p-2">About</Link>
+               <Link href="/services" className="block p-2">Services</Link>
+               <Link href="/engagement" className="block p-2">Engagement</Link>
+               <Link href="/pricing" className="block p-2">Pricing</Link>
+               <Link href="/faq" className="block p-2">FAQ</Link>
+        </MobileNavbar>
     </header>
   );
 }
